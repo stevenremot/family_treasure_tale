b7d1f078c8612b52eb01e4467b5c97c1f4c16e91
Steven Rémot <steven.remot@gmail.com>
2014-10-10 22:14:20 +0200

    Remove double speed hack

--- family_treasure/game.py
+++ family_treasure/game.py
@@ -72,7 +72,7 @@ class Game:
                     mouse_system.on_mouse_motion(event.pos)
 
             clock.tick(self.fps)
-            time_elapsed = float(clock.get_time()) / 1000.0 * 2.0 # Remove multiplicator
+            time_elapsed = float(clock.get_time()) / 1000.0
 
             scheduler.update(time_elapsed)
             animation_system.update(time_elapsed)
